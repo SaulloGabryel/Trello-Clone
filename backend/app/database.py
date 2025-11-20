28550deebe78a1488c258c0c4537246e68619117
Saullo Gabryel <saullo.g.dev@gmail.com>
2025-11-20 19:53:30 -0300

    fix: delete .env and trade engine location

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,8 +1,8 @@
 from sqlalchemy import create_engine, Column, String, Integer
 from sqlalchemy.orm import declarative_base
 from datetime import datetime
-
+from ..env import database_url
 
 Base = declarative_base()
 
-engine = create_engine("mysql+pymysql://root:%23Digimon1230@localhost/trello_clone")
+engine = create_engine(database_url)
